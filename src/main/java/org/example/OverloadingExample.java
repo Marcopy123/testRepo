6e2e2da7b15c922ce062e031a5105d62852a1bae
marcopy123 <dollartheque@yahoo.com>
2024-02-08 15:10:14 -0500

    aaa

---
+++ src/main/java/org/example/OverloadingExample.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class OverloadingExample {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add (int a, int b, int c) {
+        return a + b + c;
+    }
+}
